f88267812c843a05e58612b450ace6ede1341b3e
Balazs Hevesi <bh222pv@student.lnu.se>
2026-02-18 14:03:10 +0100

    Move colemak layout to karabiner elements

--- features/keyboard/darwin.nix
+++ features/keyboard/darwin.nix
@@ -1,12 +1,4 @@
-{ lib, ... }: {
-  system.activationScripts.preActivation.text = lib.mkAfter ''
-    install -d -m 0755 "/Library/Keyboard Layouts"
-    rm -rf "/Library/Keyboard Layouts/SwedishColemakDHk.bundle"
-    cp -R ${./SwedishColemakDHk.bundle} "/Library/Keyboard Layouts/SwedishColemakDHk.bundle"
-    chown -R root:wheel "/Library/Keyboard Layouts/SwedishColemakDHk.bundle"
-    chmod -R go-w "/Library/Keyboard Layouts/SwedishColemakDHk.bundle"
-  '';
-
+{ ... }: {
   system.defaults.CustomUserPreferences."com.apple.HIToolbox" = {
     AppleEnabledInputSources = [
       {
@@ -15,8 +7,7 @@
       }
       {
         InputSourceKind = "Keyboard Layout";
-        "KeyboardLayout ID" = -20197;
-        "KeyboardLayout Name" = "Swedish Colemak DHk";
+        InputSourceID = "com.apple.keylayout.Swedish";
       }
       {
         InputSourceKind = "Non Keyboard Input Method";
@@ -27,8 +18,7 @@
     AppleSelectedInputSources = [
       {
         InputSourceKind = "Keyboard Layout";
-        "KeyboardLayout ID" = -20197;
-        "KeyboardLayout Name" = "Swedish Colemak DHk";
+        InputSourceID = "com.apple.keylayout.Swedish";
       }
     ];
   };
