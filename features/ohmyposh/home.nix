27d0648a78f69cad550c467e9ec636b448bf660a
Balazs Hevesi <bh222pv@student.lnu.se>
2026-02-22 13:28:41 +0100

    Turn omp into json

--- features/ohmyposh/home.nix
+++ features/ohmyposh/home.nix
@@ -1,11 +1,6 @@
-{ lib, pkgs, ... }:
-let
-  ohMyPoshConfig = import ./config/settings.nix;
-in {
-  home.activation.configureOhMyPosh = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    ohmyposh_dir="$HOME/.config/ohmyposh"
-    mkdir -p "$ohmyposh_dir"
-
-    install -m 0644 ${pkgs.writeText "ohmyposh-config.json" (builtins.toJSON ohMyPoshConfig)} "$ohmyposh_dir/config.omp.json"
-  '';
+{ ... }: {
+  home.file.".config/ohmyposh/config.omp.json" = {
+    source = ./config/settings.json;
+    force = true;
+  };
 }
